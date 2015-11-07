fc787a5ee7a62e62755778f6fe12b3c777647f1d
andrey.leskov <andrey.leskov@gmail.com>
2015-11-08 01:27:35 +0300

    Script for filenames renaming from CP866 as UTF8

---
+++ BitcasaCP866ToUnicodeRenaming.ps1
@@ -0,0 +1,28 @@
+﻿ function ConvertTo-Encoding ([string]$From, [string]$To){
+	Begin{
+		$encFrom = [System.Text.Encoding]::UTF8
+		$encTo = [System.Text.Encoding]::GetEncoding("CP866")
+	}
+	Process{
+		$bytes = $encTo.GetBytes($_)
+		$bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
+		$encTo.GetString($bytes)
+	}
+}
+
+foreach( $item in Get-ChildItem -Recurse | Sort-Object -Property FullName –Descending){
+
+ $correctName = $item.Name | ConvertTo-Encoding "Unicode" "CP866"
+ if($correctName.Contains("???") -or $correctName -eq $item.Name) {continue}
+
+ $itemParentPath = $item.FullName.Remove($item.FullName.Length - $item.Name.Length);
+ $correctFullName = $itemParentPath + $correctName
+
+  "renaming" 
+  $item.FullName
+  "to"
+  $correctFullName
+
+  Rename-Item $item.FullName $correctFullName
+ #}
+}
